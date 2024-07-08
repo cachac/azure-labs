d524fea8ef3d1ece208577b5bf1b23433d99ca5b
Car <carlos@storylabs.dev>
2024-07-08 09:30:49 -0600

    azure

--- README.md
+++ README.md
@@ -1,4 +1,41 @@
 # Azure-labs <!-- omit in toc -->
 Laboratorios para prácticas en Azure
 
+# 1. Virtual machine
+## 1.1. Basics
+- Create Azure Virtual Machine
+- Resourse group: rg-<CURSO>-<ESTUDIANTE>
+- Name: vm<CURSO><ESTUDIANTE>
+- Region: East US
+- Image: Ubuntu server 22.04 LTS - x64 Gen2
+- Size: Standard_B2s - 2vcpus, 4 GiB memory ($30/mo)
 
+# 2. Review + create
+- Create
+- Download private key and create resource
+
+
+# 3. Conectar a la terminal
+- Goto resource
+## 3.1. Overview
+- Copiar la IP pública
+
+## 3.2. Connect Azure CLI (Portal)
+- Connect
+- SSH using Azure CLI
+
+## 3.3. Opcional SSH por terminal
+```
+ssh -i <KEY>.pem azureuser@<PUBLIC_IP>
+```
+
+# 4. Setup lab
+
+
+
+
+
+
+
+
+security group
---
+++ script.sh
@@ -0,0 +1,22 @@
+sudo su
+echo -e "password\npassword\n" | passwd azureuser
+su azureuser
+cd /home/azureuser
+
+sudo apt update
+
+# Install zsh
+sudo apt install zsh -y
+/bin/bash -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)" \ </dev/null
+
+git clone https://github.com/zsh-users/zsh-autosuggestions /home/azureuser/.zsh/zsh-autosuggestions
+echo 'source ~/.zsh/zsh-autosuggestions/zsh-autosuggestions.zsh' >> /home/azureuser/.zshrc
+
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git /home/azureuser/.zsh/zsh-syntax-highlighting
+echo "source ~/.zsh/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> /home/azureuser/.zshrc
+
+git clone https://github.com/zsh-users/zsh-history-substring-search /home/azureuser/.oh-my-zsh/custom/plugins/zsh-history-substring-search
+sed -i "s/plugins=(git)/plugins=(git zsh-history-substring-search)/g" /home/azureuser/.zshrc
+
+# Install Nginx + Code Server
+sudo apt install -y nginx
